f92b08589eeefb23966b2e639dc53d866bad3bde
YihengZhang <1229914511@qq.com>
2026-01-10 15:59:26 +0800

    Create bash_ppo.sh

---
+++ bash_ppo.sh
@@ -0,0 +1,51 @@
+time=$(date '+%Y-%m-%d-%H%M%S')
+start_time=$(date +%s)
+
+source activate ano_trl
+
+
+id=3
+time=$(date '+%Y-%m-%d-%H%M%S')
+/bin/echo "${id} PPO"
+export CUDA_VISIBLE_DEVICES=$id
+# CUDA_VISIBLE_DEVICES=$id 
+echo "Starting PPO with Reduced Batch Size (8)..."
+accelerate launch \
+    --config_file examples/accelerate_configs/deepspeed_zero2.yaml \
+    --num_processes 1 \
+    examples/scripts/ppo/ppo_tldr.py \
+    --num_sample_generations 0 \
+    --output_dir models/minimal/ppo_tldr \
+    --learning_rate 3e-6 \
+    --per_device_train_batch_size 8 \
+    --gradient_accumulation_steps 8 \
+    --total_episodes 1000000 \
+    --model_name_or_path EleutherAI/pythia-1b-deduped \
+    --sft_model_path cleanrl/EleutherAI_pythia-1b-deduped__sft__tldr \
+    --reward_model_path cleanrl/EleutherAI_pythia-1b-deduped__reward__tldr \
+    --local_rollout_forward_batch_size 8 \
+    --missing_eos_penalty 1.0 \
+    --stop_token eos \
+    --dataset_name "TRL-Lib/tldr" > train_PPO_$time.txt 2>&1
+
+
+    #!/bin/bash
+
+
+
+end_time=$(date +%s)
+duration=$((end_time - start_time))
+
+# 计算小时、分钟、秒
+hours=$((duration / 3600))
+minutes=$(( (duration % 3600) / 60 ))
+seconds=$((duration % 60))
+
+printf "脚本运行时间: "
+if [ $hours -gt 0 ]; then
+    printf "%02d小时 " $hours
+fi
+if [ $minutes -gt 0 ] || [ $hours -gt 0 ]; then
+    printf "%02d分钟 " $minutes
+fi
+printf "%02d秒\n" $seconds
