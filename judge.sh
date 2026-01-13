1ee1afe8125f64a5466e3e878750dd8c7e5a9737
YihengZhang <1229914511@qq.com>
2026-01-14 02:00:12 +0800

    Update judge.sh

--- judge.sh
+++ judge.sh
@@ -5,7 +5,7 @@ source activate ano_trl
 export CUDA_VISIBLE_DEVICES=0
 
 python judge.py \
-    --model_a_path "/home/yiheng/trl/models/minimal/ano_0.2_0.05_tldr_2026-01-13-115400/checkpoint-1000" \
-    --model_b_path "/home/yiheng/trl/models/minimal/ppo_r" \
+    --model_a_path "models/minimal/ano_0.2_0.05_tldr_2026-01-13-115400/checkpoint-1000" \
+    --model_b_path "models/minimal/ppo_r" \
     --num_examples 100 \
     --batch_size 16
