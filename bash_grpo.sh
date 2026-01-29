160309674b7f2cfd4467d3fa54f40ffcccde4522
YihengZhang <1229914511@qq.com>
2026-01-29 17:52:05 +0800

    Update bash_grpo.sh

--- bash_grpo.sh
+++ bash_grpo.sh
@@ -15,11 +15,12 @@ accelerate launch \
     --config_file examples/accelerate_configs/deepspeed_zero2.yaml \
     --num_processes 1 \
     examples/scripts/grpo/grpo_tldr.py \
-    --output_dir models/minimal/grpo_tldr_${time} \
+    --output_dir models/minimal/gano_tldr_${time} \
     --learning_rate 3e-6 \
-    --per_device_train_batch_size 2 \
+    --per_device_train_batch_size 16 \
     --num_generations 4 \
-    --gradient_accumulation_steps 8 \
+    --gradient_accumulation_steps 4 \
+    --num_iterations 1 \
     --max_steps 15625 \
     --model_name_or_path cleanrl/EleutherAI_pythia-1b-deduped__sft__tldr \
     --reward_model_path cleanrl/EleutherAI_pythia-1b-deduped__reward__tldr \
@@ -29,6 +30,7 @@ accelerate launch \
     --max_completion_length 53 > train_GRPO_${time}.txt 2>&1
 
 
+
     #!/bin/bash
 
 
@@ -36,16 +38,15 @@ accelerate launch \
 end_time=$(date +%s)
 duration=$((end_time - start_time))
 
-# 计算小时、分钟、秒
 hours=$((duration / 3600))
 minutes=$(( (duration % 3600) / 60 ))
 seconds=$((duration % 60))
 
-printf "脚本运行时间: "
+printf "Running Time: "
 if [ $hours -gt 0 ]; then
-    printf "%02d小时 " $hours
+    printf "%02dH " $hours
 fi
 if [ $minutes -gt 0 ] || [ $hours -gt 0 ]; then
-    printf "%02d分钟 " $minutes
+    printf "%02dM " $minutes
 fi
-printf "%02d秒\n" $seconds
+printf "%02dS\n" $seconds
