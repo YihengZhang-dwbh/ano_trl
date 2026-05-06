f328a397a3ea6ce48b081926c67ebc880573aee7
YihengZhang <1229914511@qq.com>
2026-05-06 19:44:46 +0800

    Update judge.sh

--- judge.sh
+++ judge.sh
@@ -1,18 +1,15 @@
 temp=0
-step=500
 num=100
 
 source activate ano_trl
 
 export CUDA_VISIBLE_DEVICES=0,1
 
-ano=ano/checkpoint-$step
-ano_f=ano/
+ano="your_anon_checkpoint-step"
 
-ppo=ppo/checkpoint-$step
-ppo_f=ppo/
+ppo="your_ppo_checkpoint-step"
 
-python judge_tie.py \
+python judge.py \
     --model_a_path $ano \
     --model_b_path $ppo \
     --num_examples $num \
